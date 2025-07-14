02fab1d37337f64c476912a727a6671318879a34
Evgeniy Nikulin <evgeniy.nikulin.1991@gmail.com>
2025-07-14 17:13:28 +0500

    Initial commit

---
+++ src/test/java/com/github/Evgenn91/eduka/EdukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Evgenn91.eduka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
